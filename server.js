4c56d2b09d5498d8e8f8d56b7120ce330f619e4c
mohamedazizchebil <chebilaziz461@gmail.com>
2025-07-10 10:53:12 +0100

    refactor: enable credentials in CORS middleware for enhanced security

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(cors({
     origin: true, 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: false, 
+  credentials: true, 
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
